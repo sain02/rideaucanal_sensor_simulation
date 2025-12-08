b7070916325d766a1b5a0364f8a1f37968ae81e7
Sain0225 <sainisaizal@gmail.com>
2025-12-08 15:05:58 -0500

    commit changes

--- sensoresimulator.py
+++ sensoresimulator.py
@@ -1,41 +1,27 @@
-import os
 import json
-import random
 import time
+import random
 from datetime import datetime
-from dotenv import load_dotenv
 from azure.iot.device import IoTHubDeviceClient, Message
 
-load_dotenv()
+CONNECTION_STRING = "YOUR_DEVICE_CONNECTION_STRING"
 
-CONNECTION_STRING = os.getenv("IOTHUB_DEVICE_CONNECTION_STRING")
+client = IoTHubDeviceClient.create_from_connection_string(CONNECTION_STRING)
 
-# Three locations required for dashboard
-LOCATIONS = ["DowsLake", "FifthAvenue", "NAC"]
+locations = ["DowsLake", "FifthAvenue", "NAC"]
 
-def generate_sensor_data(location):
-    return {
-        "location": location,
-        "timestamp": datetime.utcnow().isoformat() + "Z",
-        "iceThickness": round(random.uniform(20, 40), 2),
-        "surfaceTemperature": round(random.uniform(-15, 2), 2),
-        "snowAccumulation": round(random.uniform(0, 10), 2),
-        "externalTemperature": round(random.uniform(-20, 5), 2)
-    }
+while True:
+    for loc in locations:
+        data = {
+            "location": loc,
+            "iceThickness": round(random.uniform(28, 35), 2),
+            "surfaceTemp": round(random.uniform(-12, 2), 2),
+            "snow": round(random.uniform(5, 12), 2),
+            "timestamp": datetime.utcnow().isoformat() + "Z"
+        }
 
-def main():
-    print("Connecting to Azure IoT Hub...")
-    client = IoTHubDeviceClient.create_from_connection_string(CONNECTION_STRING)
-    print("Connected! Sending messages every 10 seconds...")
+        msg = Message(json.dumps(data))
+        client.send_message(msg)
+        print("Sent:", data)
 
-    while True:
-        for loc in LOCATIONS:
-            data = generate_sensor_data(loc)
-            message = Message(json.dumps(data))
-            client.send_message(message)
-            print("Sent:", data)
-
-        time.sleep(10)
-
-if __name__ == "__main__":
-    main()
+    time.sleep(10)
